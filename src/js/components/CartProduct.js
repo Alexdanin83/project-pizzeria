26effd66c69717ed622b078190e96a4a18519d3f
Alex Danin <alexey.danin@gmail.com>
2020-11-30 18:13:36 +0100

    fix heroku

--- src/js/components/CartProduct.js
+++ src/js/components/CartProduct.js
@@ -1,5 +1,5 @@
 import {select} from '../settings.js';
-import amountWidget from './amountWidget.js';
+import amountWidget from './AmountWidget.js';
 class CartProduct{
   constructor(menuProduct, element){
     const thisCartProduct = this;
